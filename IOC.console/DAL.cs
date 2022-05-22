78c7939a2bc3f0dc22bb865610c68f58a7881af2
dogustuluk <dogus.tuluk@gmail.com>
2022-05-22 11:14:05 +0300

    DAL and Product classes fixed

--- IOC.console/DAL.cs
+++ IOC.console/DAL.cs
@@ -15,7 +15,7 @@ namespace IOC.console
                 new Product{Id = 1, Name = "Kalem 1", Price = 100, Stock = 10},
                 new Product{Id = 2, Name = "Kalem 2", Price = 200, Stock = 20},
                 new Product{Id = 3, Name = "Kalem 3", Price = 300, Stock = 30},
-                new Product{Id = 4, Name = "Kalem 4", Price = 400, Stock = 40},
+                new Product{Id = 4, Name = "Kalem 4", Price = 400, Stock = 40}
 
 
             };
--- IOC.console/Product.cs
+++ IOC.console/Product.cs
@@ -10,7 +10,7 @@ namespace IOC.console
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public decimal Price< { get; set; }
+        public decimal Price { get; set; }
         public int Stock { get; set; }
     }
 }
