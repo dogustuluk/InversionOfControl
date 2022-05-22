63319b9fd1555d28c407a31deba5eb34829fafb6
dogustuluk <dogus.tuluk@gmail.com>
2022-05-22 13:13:45 +0300

    DALFactory

--- IOC.console/BL.cs
+++ IOC.console/BL.cs
@@ -11,7 +11,7 @@ namespace IOC.console
         private DAL _dal { get; set; }
         public BL()
         {
-            _dal = new DAL();
+            _dal = new DALFactory.GetDal();
         }
 
         public List<Product> GetProducts()
