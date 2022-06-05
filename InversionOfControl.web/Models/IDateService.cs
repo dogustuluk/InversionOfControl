742bdb5a0b818862da3e5cd53ac0ea610b2cf2c2
dogustuluk <dogus.tuluk@gmail.com>
2022-06-05 22:08:49 +0300

    DateService, IDateService, Startup class created

---
+++ InversionOfControl.web/Models/IDateService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InversionOfControl.web.Models
+{
+    interface IDateService
+    {
+        DateTime GetDateTime { get; }
+    }
+    
+     interface ISingletonDateService: IDateService { }
+     interface IScopedDateService: IDateService { }
+     interface ITransientDateService: IDateService { }
+}
